492869b9e2bc0d07636d2c9ad4326d9887b47d01
Sajib Saha <sajib.saha@brainstation-23.com>
2024-10-02 11:44:23 +0600

    Created carts controller | Added functional carts icon to the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "products#index"
 
+  get "carts/:id", to: "carts#show", as: "carts"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
