94cea484331c93b67752c1895b952d8dcab1e6d5
Sajib Saha <sajib.saha@brainstation-23.com>
2024-10-02 12:03:23 +0600

    Buy now button added to the product details page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get "carts/:id", to: "carts#show", as: "carts"
 
+  post "line_items_buy_now/:product_id", to: "line_items#buy_now", as: "line_item_buy_now"
+  post "line_items_add_to_cart/:product_id", to: "line_items#add_to_cart", as: "line_item_add_to_cart"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
